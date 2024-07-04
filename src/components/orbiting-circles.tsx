b93c2cded06ebb77decb2300efe4315e726ebe1f
Rahul_Singh <koderrahul@gmail.com>
2024-07-04 16:50:55 +0530

    final changes

--- src/components/orbiting-circles.tsx
+++ src/components/orbiting-circles.tsx
@@ -6,7 +6,7 @@ export function OrbitingCirclesDemo() {
   return (
     <div className="relative flex h-[500px] w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-sm">
       <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-        <Image src={companyLogo} />
+        <Image src={companyLogo} alt="image" />
       </span>
 
       {/* Inner Circles */}
